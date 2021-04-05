479ea036d5f19292df99e8801e672fc234dea66a
Storms <Storms@Alex>
2021-04-05 16:57:22 -0500

    Examen de programacion del parcial 2

---
+++ src/examen/poo/segundo/parcial/ExamenPOOSegundoParcial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen.poo.segundo.parcial;
+
+/**
+ *
+ * @author Storms
+ */
+public class ExamenPOOSegundoParcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
